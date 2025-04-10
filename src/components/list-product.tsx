162cdd842570df135bcd068e499d49183a52fdf6
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-04-10 23:29:10 +0900

    Detail Skeleton

--- src/components/list-product.tsx
+++ src/components/list-product.tsx
@@ -16,7 +16,7 @@ export default function ListProduct({ title, price, created_at, photo, id }: Lis
       <div className="relative size-28 rounded-md overflow-hidden">
         <Image fill src={photo} alt={title} />
       </div>
-      <div className="flex flex-col gap-1 *:text-white">
+      <div className="flex flex-col gap-1">
         <span className="text-lg">{title}</span>
         <span className="text-sm text-neutral-500">{formatToTimeAgo(created_at.toString())}</span>
         <span className="text-lg font-semibold">{formatToWon(price)}원</span>
